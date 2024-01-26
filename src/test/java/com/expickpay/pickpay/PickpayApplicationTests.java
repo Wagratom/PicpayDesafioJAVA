f22a14523dfd9d094ae980c5eccee1b6c3bb6fd0
Bankai <wagratom22@gmail.com>
2024-01-26 16:51:35 -0300

    firts commit

---
+++ src/test/java/com/expickpay/pickpay/PickpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expickpay.pickpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
