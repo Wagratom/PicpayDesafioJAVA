9f5b6e368e02a275824394a1000818ebe7d39990
Bankai <wagratom22@gmail.com>
2024-01-27 16:55:16 -0300

    create new controllers routes POST/GET

--- src/main/java/com/expickpay/pickpay/contollers/UserControllers.java
+++ src/main/java/com/expickpay/pickpay/contollers/UserControllers.java
@@ -1,20 +1,33 @@
 package com.expickpay.pickpay.contollers;
 
+import com.expickpay.pickpay.dtos.UserDto;
+import com.expickpay.pickpay.entity.UserEntity;
+import com.expickpay.pickpay.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserControllers {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    private final UserService userService;
+    @Autowired
+    public UserControllers(UserService userService){
+        this.userService = userService;
+    }
+    @GetMapping("/")
+    public ResponseEntity<Iterable<UserEntity>> getAllUsers() {
+        List<UserEntity> allUsers = this.userService.getAllUsers();
+        return ResponseEntity.ok(allUsers);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<UserEntity> hello(@RequestBody UserDto dto) {
+        System.out.println(dto);
+        UserEntity newUser = this.userService.createUser(dto);
+       return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
-    @GetMapping("/hello2")
-    public ResponseEntity<String> hello2(){
-        return ResponseEntity.ok("Hello World");
-    }
 }
