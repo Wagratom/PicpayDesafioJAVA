91a28362b6a1a9f741baa10863ff0980d4ab4822
Bankai <wagratom22@gmail.com>
2024-01-27 17:09:40 -0300

    created UserService with GetAll/POST

---
+++ src/main/java/com/expickpay/pickpay/service/UserService.java
@@ -0,0 +1,26 @@
+package com.expickpay.pickpay.service;
+
+import com.expickpay.pickpay.dtos.UserDto;
+import com.expickpay.pickpay.entity.UserEntity;
+import com.expickpay.pickpay.reposity.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UsersRepository usersRepository;
+    @Autowired
+    public UserService(UsersRepository usersRepository){
+        this.usersRepository = usersRepository;
+    }
+
+    public UserEntity createUser(UserDto dto) {
+        return this.usersRepository.save(new UserEntity(dto));
+    }
+
+    public List<UserEntity> getAllUsers() {
+        return this.usersRepository.findAll();
+    }
+}
