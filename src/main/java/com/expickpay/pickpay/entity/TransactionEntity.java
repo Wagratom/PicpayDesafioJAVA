d411b80ab6ff7ad391767c271804fe8c91a38c05
Bankai <wagratom22@gmail.com>
2024-01-27 12:49:07 -0300

    add Transaction entity

---
+++ src/main/java/com/expickpay/pickpay/entity/TransactionEntity.java
@@ -0,0 +1,32 @@
+package com.expickpay.pickpay.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private UserEntity sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private UserEntity receiver;
+
+    private double amount;
+    private LocalDateTime date;
+}
