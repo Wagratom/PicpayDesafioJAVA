8b719cf86fd8171d1bc9a0aca1a50655d7d84fd0
Bankai <wagratom22@gmail.com>
2024-01-28 12:58:43 -0300

    added route to execute a new transactions

--- src/main/java/com/expickpay/pickpay/contollers/TransactionControllers.java
+++ src/main/java/com/expickpay/pickpay/contollers/TransactionControllers.java
@@ -1,14 +1,27 @@
 package com.expickpay.pickpay.contollers;
 import com.expickpay.pickpay.dtos.TransactionDTO;
+import com.expickpay.pickpay.entity.TransactionEntity;
+import com.expickpay.pickpay.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("transaction")
 public class TransactionControllers {
-    @PostMapping("/transaction")
-    public ResponseEntity<String> ExecuteTransaction(@RequestBody TransactionDTO dto) {
-        return ResponseEntity.ok("Transaction created");
+
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionControllers(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @PostMapping("/newTransaction")
+    public ResponseEntity<TransactionEntity> ExecuteTransaction(@RequestBody TransactionDTO dto) {
+        TransactionEntity transaction = this.transactionService.createTransaction(dto);
+        return ResponseEntity.ok(transaction);
     }
 }
