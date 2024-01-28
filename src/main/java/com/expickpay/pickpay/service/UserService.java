816563d2714534a53fd8815a732fb560f904bb20
Bankai <wagratom22@gmail.com>
2024-01-28 12:52:45 -0300

    implements new functionalities on UserService

--- src/main/java/com/expickpay/pickpay/service/UserService.java
+++ src/main/java/com/expickpay/pickpay/service/UserService.java
@@ -3,10 +3,12 @@ package com.expickpay.pickpay.service;
 import com.expickpay.pickpay.dtos.UserDto;
 import com.expickpay.pickpay.entity.UserEntity;
 import com.expickpay.pickpay.excecoes.InvalidIdException;
+import com.expickpay.pickpay.excecoes.UserNotFoundException;
 import com.expickpay.pickpay.reposity.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -26,8 +28,31 @@ public class UserService {
     }
 
     public UserEntity getUserById(Long id) {
-        return this.usersRepository.findById(id).orElseThrow(
-                ()-> new InvalidIdException("Invalid id")
+        if (id == null) {
+            throw new InvalidIdException("Error: getUserById: Id is null or invalid");
+        }
+        return this.usersRepository.findById(id).orElseThrow (
+                ()-> new UserNotFoundException("Invalid id")
         );
+    }
+
+    public UserEntity getReferrerById(Long id) {
+        if (id == null) {
+            throw new InvalidIdException("Error: getReferrerById: Id is null or invalid");
+        }
+        if (!this.usersRepository.existsById(id)) {
+            throw new UserNotFoundException("Error: getReferrerById: User not found");
+        }
+        return this.usersRepository.getReferenceById(id);
+    }
+    public void updateAmount(UserEntity user, BigDecimal amount){
+        if (user == null) {
+            throw new IllegalArgumentException("Error: updateAmount: updateAmount: User is null or invalid");
+        }
+        if (amount == null) {
+            throw new IllegalArgumentException("Error: updateAmount: updateAmount: Amount is null or invalid");
+        }
+        user.setAmount(user.getAmount().subtract(amount));
+        this.usersRepository.save(user);
     }
 }
