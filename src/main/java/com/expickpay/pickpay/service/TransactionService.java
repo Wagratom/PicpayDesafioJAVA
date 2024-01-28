87e39dc382ebd6ae610efd0bd9d4050718342266
Bankai <wagratom22@gmail.com>
2024-01-28 14:28:39 -0300

    added a new API to validate the transaction

--- src/main/java/com/expickpay/pickpay/service/TransactionService.java
+++ src/main/java/com/expickpay/pickpay/service/TransactionService.java
@@ -6,15 +6,21 @@ import com.expickpay.pickpay.entity.UserEntity;
 import com.expickpay.pickpay.excecoes.InvalidTransactionException;
 import com.expickpay.pickpay.reposity.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final UserService userService;
+
+    private final RestTemplate restTemplate = new RestTemplate();
     @Autowired
     public TransactionService(TransactionRepository transactionRepository, UserService userService) {
         this.transactionRepository = transactionRepository;
@@ -26,14 +32,14 @@ public class TransactionService {
         UserEntity userSender = this.userService.getUserById(dto.senderId());
 
         isValidSender(userSender, dto.amount());
+        validTransactionOnAPI();
 
         TransactionEntity transaction = new TransactionEntity();
-
         transaction.setAmount(dto.amount());
         transaction.setSender(userSender);
         transaction.setReceiver(userReceiver);
         transaction.setDate(LocalDateTime.now());
-        this.userService.updateAmount(userSender, dto.amount());
+        this.userService.updateAmount(userSender, userReceiver, dto.amount());
         return this.transactionRepository.save(transaction);
     }
 
@@ -44,5 +50,19 @@ public class TransactionService {
         if (user.getAmount().compareTo(amount) < 0) {
             throw new InvalidTransactionException("Insufficient funds");
         }
+    }
+
+    public void validTransactionOnAPI() {
+        ResponseEntity<Map> AuthorizationResponse = this.restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+        System.out.println(AuthorizationResponse);
+        System.out.println();
+        System.out.println(AuthorizationResponse.getBody());
+        if (AuthorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String messageResponse = (String) AuthorizationResponse.getBody().get("message");
+            if (messageResponse != null && messageResponse.equals("Autorizado")) {
+                return;
+            }
+        }
+        throw new InvalidTransactionException("Transaction not authorized by the API");
     }
 }
